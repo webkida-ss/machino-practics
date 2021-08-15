226d71fc8b6aedff1277e45d4df79bb0015ea2c9
webkida-ss <webkida.production@gmail.com>
2021-08-15 14:48:48 +0900

    ウィジェット設定

--- src-php/99_functions/action-hook/_widgets_init.php
+++ src-php/99_functions/action-hook/_widgets_init.php
@@ -3,15 +3,17 @@
 /**
  * ウィジェットの登録
  */
-add_action('widgets_init', function () {
+add_action(
+	'widgets_init',
+	function () {
 		register_sidebar(
 			array(
 				'name' => 'サイドバー', // 表示するエリア名
 				'id' => 'sidebar', // id
-				// 'before_widget' => '<div id="%1$s" class="widget %2$s">',
-				// 'after_widget' => '</div>',
-				// 'before_title' => '<div class="widget-title">',
-				// 'after_title' => '</div>',
+				'before_widget' => '<section id="%1$s" class="widget %2$s">',
+				'after_widget' => '</section>',
+				'before_title' => '<h3 class="sidebar-title">',
+				'after_title' => '</h3>',
 			)
 		);
 	}
