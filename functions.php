f907094822ff2b41fb69d62da7a1d4212c7e16d9
webkida-ss <webkida.production@gmail.com>
2021-08-17 09:07:15 +0900

    jquery読み込み

--- functions.php
+++ functions.php
@@ -15,3 +15,15 @@ get_template_part('src-php/99_functions/action-hook/_wp_enqueue_scripts');
 get_template_part('src-php/99_functions/_filter-hook'); // フィルターフック
 get_template_part('src-php/99_functions/_short-code'); // ショートコード
 get_template_part('src-php/99_functions/_user-define'); // ユーザ定義関数
+
+// function mvwpform_autop_filter()
+// {
+// 	if (class_exists('MW_WP_Form_Admin')) {
+// 		$mw_wp_form_admin = new MW_WP_Form_Admin();
+// 		$forms = $mw_wp_form_admin->get_forms();
+// 		foreach ($forms as $form) {
+// 			add_filter('mwform_content_wpautop_mw-wp-form-' . $form->ID, '__return_false');
+// 		}
+// 	}
+// }
+// mvwpform_autop_filter();
