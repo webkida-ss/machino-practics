f623ba1d043486445d3f04cdb19300025e94675f
webkida-ss <webkida.production@gmail.com>
2021-08-19 16:23:19 +0900

    院長紹介ボタン

--- src-php/20_component/04_introduction/_introduction-director.php
+++ src-php/20_component/04_introduction/_introduction-director.php
@@ -6,7 +6,7 @@ $post_id = $post->ID;
 
 <section class="introduction-director">
 	<h3 class="section__title">院長挨拶</h3>
-	<div class="introduction-director__content">
+	<div class="introduction-director__container">
 		<div class="introduction-director__img">
 			<img src="<?php the_field('director_img', $post_id); ?>" alt="院長画像">
 		</div>
@@ -14,4 +14,11 @@ $post_id = $post->ID;
 			<?php the_field('director_msg', $post_id); ?>
 		</div>
 	</div>
+	<div class="util__link introduction-director__link">
+		<a href="<?php echo esc_url(home_url() . '/introduction'); ?>">
+			<span>店舗紹介を見る</span>
+			<span>店舗紹介を見る</span>
+		</a>
+	</div>
+
 </section>
