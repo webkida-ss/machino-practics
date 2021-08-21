2ba4bcc3a8d70f63e8ff16a2e99a2d22c7bded73
webkida-ss <webkida.production@gmail.com>
2021-08-21 10:35:12 +0900

    トップメニュー

--- src-php/10_common/_footer.php
+++ src-php/10_common/_footer.php
@@ -1,18 +1,22 @@
 <footer class="footer">
 	<div class="footer__upper">
-		<div class="footer__upper--img">
-			<a href="<?php echo esc_url(home_url()) ?>">
-				<img src="<?php echo esc_url(get_template_directory_uri() . '/dist/img/ロゴ-透明.png'); ?>" alt="ロゴ">
-			</a>
-		</div>
-		<ul class="inner footer__upper--list">
-			<li><a href="<?php echo esc_url(home_url()); ?>">トップページ</a></li>
-			<li><a href="<?php echo esc_url(home_url() . '/news'); ?>">お知らせ</a></li>
-			<li><a href="<?php echo esc_url(home_url() . '/menu'); ?>">メニュー</a></li>
-			<li><a href="<?php echo esc_url(home_url() . '/introduction'); ?>">店舗紹介</a></li>
-			<li><a href="<?php echo esc_url(home_url() . '/qa'); ?>">Q&A</a></li>
-			<li><a href="<?php echo esc_url(home_url() . '/contact'); ?>">お問い合わせ</a></li>
-		</ul>
+		<div class="inner footer__upper--container">
+			<div class="footer__upper--img">
+				<a href="<?php echo esc_url(home_url()) ?>">
+					<img src="<?php echo esc_url(get_template_directory_uri() . '/dist/img/ロゴ-透明.png'); ?>" alt="ロゴ">
+				</a>
+			</div>
+			<div class="footer__upper--list">
+				<ul>
+					<li><a href="<?php echo esc_url(home_url()); ?>">トップページ</a></li>
+					<li><a href="<?php echo esc_url(home_url() . '/news'); ?>">お知らせ</a></li>
+					<li><a href="<?php echo esc_url(home_url() . '/menu'); ?>">メニュー</a></li>
+					<li><a href="<?php echo esc_url(home_url() . '/introduction'); ?>">店舗紹介</a></li>
+					<li><a href="<?php echo esc_url(home_url() . '/qa'); ?>">Q&A</a></li>
+					<li><a href="<?php echo esc_url(home_url() . '/contact'); ?>">お問い合わせ</a></li>
+				</ul>
+			</div>
+		</div><!-- /.footer__upper--container -->
 	</div>
 	<div class="footer__lower">
 		<p class="footer__copy-right">
