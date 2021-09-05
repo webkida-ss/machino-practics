d376d6b410f460736cdba12946a2370d19990ae7
webkida-ss <webkida.production@gmail.com>
2021-09-05 14:32:12 +0900

    product-item修正

--- src-php/20_component/06_product/_product-item.php
+++ src-php/20_component/06_product/_product-item.php
@@ -1,6 +1,6 @@
-<li class="news-item">
-	<a href="<?php the_permalink(); ?>" class="news-item__link">
-		<time class="news-item__date"><?php echo get_the_date(); ?></time>
-		<p class="news-item__title"><?php the_title(); ?></p>
+<li class="product-item">
+	<a href="<?php the_permalink(); ?>" class="product-item__link">
+		<time class="product-item__date"><?php echo get_the_date(); ?></time>
+		<p class="product-item__title"><?php the_title(); ?></p>
 	</a>
 </li>
